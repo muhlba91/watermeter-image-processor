4370fa5da393bfe8a89de77f2a4955d501848fbe
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-05-04 09:23:27 +0200

    fix: fix golangci-lint errors; update dependencies

---
+++ internal/health/constants.go
@@ -0,0 +1,7 @@
+package health
+
+// valueUp represents the "up" status for a component in the health check.
+const valueUp = "up"
+
+// valueDown represents the "down" status for a component in the health check.
+const valueDown = "down"
